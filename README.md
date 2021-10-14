db01a5bbaf236d514106bcfa3a8aca7cd20a52eb
むずでょ <muzudho1@gmail.com>
2021-10-14 22:59:13 +0900

    Link

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wpf-multithread-practice
-WPFでマルチスレッドの練習
+
+WPFでマルチスレッドの練習  
+
+📖 [増補改訂版 Java言語で学ぶデザインパターン入門 マルチスレッド編 Kindle Edition](https://www.amazon.co.jp/exec/obidos/ASIN/4797331623/aaaaab0c-22/ref=nosim/)  
+👆 このKindle本読んで勉強  
---
+++ WpfMultithreadingPractice/WpfMultithreadingPractice/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WpfMultithreadingPractice
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// [P3 single threaded]ボタン押下時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for(var i=0; i<10000; i++)
+            {
+                Trace.WriteLine("Good!");
+            }
+        }
+    }
+}
