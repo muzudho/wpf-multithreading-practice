d8cdf89d3c8032fb450a36ae8b59fe2d15512519
むずでょ <muzudho1@gmail.com>
2021-10-14 23:25:48 +0900

    P13 two threads

--- WpfMultithreadingPractice/WpfMultithreadingPractice/MainWindow.xaml.cs
+++ WpfMultithreadingPractice/WpfMultithreadingPractice/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Diagnostics;
+    using System.Threading;
     using System.Windows;
 
     /// <summary>
@@ -15,6 +16,9 @@
         }
 
         /// <summary>
+        /// List I1-1
+        /// =========
+        /// 
         /// [P3 single threaded]ボタン押下時
         /// </summary>
         /// <param name="sender"></param>
@@ -25,6 +29,73 @@
             {
                 Trace.Write("Good!");
             }
+        }
+
+        /// <summary>
+        /// List I1-3
+        /// =========
+        /// 
+        /// [P6 my thread]ボタン押下時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void P6MyThreadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 別のスレッドにやらせる仕事
+            Thread backgroundThread = new Thread(new ThreadStart(
+                () =>
+                {
+                    // 重たい処理
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        Trace.Write("Nice!");
+                    }
+                }
+            ));
+            backgroundThread.Start();
+            // ここは通り抜けます
+
+            // このスレッドでやる仕事
+            for (var i = 0; i < 10000; i++)
+            {
+                Trace.Write("Good!");
+            }
+        }
+
+        /// <summary>
+        /// [P13 two threads]ボタン押下時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void P13TwoThreadsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 別のスレッドにやらせる仕事 その1
+            new Thread(new ThreadStart(
+                () =>
+                {
+                    // 重たい処理
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        Trace.Write("Good!");
+                    }
+                }
+            )).Start();
+            // ここは通り抜けます
+
+            // 別のスレッドにやらせる仕事 その2
+            new Thread(new ThreadStart(
+                () =>
+                {
+                    // 重たい処理
+                    for (var i = 0; i < 10000; i++)
+                    {
+                        Trace.Write("Nice!");
+                    }
+                }
+            )).Start();
+            // ここは通り抜けます
+
+            // この UIスレッド は先に終わりますが、上の２つのスレッドは継続します
         }
     }
 }
